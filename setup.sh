58138b5b3829b1513f1922665212223473a75ce6
Kuldeep  Das <137069029+Kuldeepda@users.noreply.github.com>
2025-07-26 20:20:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Create streamlit config
+# Create Streamlit config directory
 mkdir -p ~/.streamlit/
+
+# Write Streamlit config
 echo "\
 [server]\n\
 headless = true\n\
 port = \$PORT\n\
 enableCORS = false\n\
 " > ~/.streamlit/config.toml
+
+# Install NLTK FIRST
+pip install nltk
 
 # Create nltk_data folder
 mkdir -p nltk_data
